9181b76dd8e71d1cb3e7597a5266068d73ed0d83
Student <student@students-mbp.lan>
2021-01-03 20:59:31 +1100

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = 'f659e4a2';
+  const API_KEY = '775ed1cd5fb9b8bbc7bcc5b1045858a8';
+  const API_REQ = 'https://api.edamam.com/search?q=chicken&app_id=${' + APP_ID + '}&app_key=${' + API_KEY + '}';
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   );
 }
