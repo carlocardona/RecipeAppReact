d55b8ea51b027702d1e9aa2fc1942ca1f6b8db04
Student <student@students-mbp.lan>
2021-01-04 11:42:00 +1100

    added API call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useEffect } from 'react';
 import './App.css';
-import React from 'react';
 
 const App = () => {
 
   const APP_ID = 'f659e4a2';
   const API_KEY = '775ed1cd5fb9b8bbc7bcc5b1045858a8';
-  const API_REQ = 'https://api.edamam.com/search?q=chicken&app_id=${' + APP_ID + '}&app_key=${' + API_KEY + '}';
+
+  useEffect( () => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+
+    const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY }`);
+    const data = await res.json();
+    console.log(data);
+  }
+
 
   return(
     <div className="App">
