f62bb326cdb8e96ccb3da7e17f6718cbeca9b24a
Student <student@students-mbp.lan>
2021-01-04 14:56:41 +1100

    working search with API

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Recipe from './Recipe';
 
 const App = () => {
 
   const APP_ID = 'f659e4a2';
   const API_KEY = '775ed1cd5fb9b8bbc7bcc5b1045858a8';
 
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState('chicken');
+
   useEffect( () => {
     getRecipes();
-  }, []);
+  }, [query] );
 
   const getRecipes = async () => {
 
-    const res = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY }`);
+    const res = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY }`);
+
     const data = await res.json();
-    console.log(data);
+    setRecipes(data.hits);
+    console.log(data.hits);
   }
 
+  const updateSearch = e => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    console.log(search);
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  }
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+
+      <form onSubmit={getSearch} className="search-form">
+
+        <input 
+        className="search-bar" 
+        type="text" 
+        value={search} 
+        onChange={updateSearch} 
+        />
+
         <button className="search-button" type="submit">Search</button>
       </form>
+
+      {recipes.map(recipe => (
+        <Recipe 
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image} 
+        ingredients={recipe.recipe.ingredients}
+        />
+      ))}
+
     </div>
   );
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,18 @@
 import React from 'react'
 
-const Recipe = ({ title,calories,image }) => {
+const Recipe = ({ title,calories,image,ingredients }) => {
 
     return (
         <div>
         <h3>{title}</h3>
+        <ol>
+            {ingredients.map(ingredient => (
+                <li>{ingredient.text}</li>
+            ))}
+        </ol>
         <p>{calories}</p>
         <img src={image} alt=""/>
+
         </div>
     );
 }
